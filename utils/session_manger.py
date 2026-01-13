390569685fe1cd28f15c832c868b685bbed79fdb
inikonoff <inikonoff@gmail.com>
2026-01-13 09:38:31 +0300

    Create session_manger.py

---
+++ utils/session_manger.py
@@ -0,0 +1,15 @@
+# utils/session_manager.py
+from datetime import datetime
+from config import DEFAULT_MODEL
+
+class UserSession:
+    def __init__(self, user_id: int):
+        self.user_id = user_id
+        self.history = []
+        self.current_model = DEFAULT_MODEL
+        self.created_at = datetime.now()
+        self.message_count = 0
+        self.last_activity = datetime.now()
+
+# Глобальный словарь сессий
+user_sessions = {}
